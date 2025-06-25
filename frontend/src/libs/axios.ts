61675a19d48e2e3e77e02fe9e28ecb3d8371d673
Aaryashkc <aaryashkc123@gmail.com>
2025-06-25 09:20:12 +0545

    baackend stuff

---
+++ frontend/src/libs/axios.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import type { AxiosInstance } from 'axios';
+
+const baseURL: string =
+  import.meta.env.MODE === 'development' ? 'http://localhost:5001/api' : '/api';
+
+export const axiosInstance: AxiosInstance = axios.create({
+  baseURL,
+  withCredentials: true,
+});
