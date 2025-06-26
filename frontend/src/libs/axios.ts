0d3ef23ba6348faf4bbc7812139534998fd82a39
Aaryashkc <aaryashkc123@gmail.com>
2025-06-26 13:01:45 +0545

    deploy ready

--- frontend/src/libs/axios.ts
+++ frontend/src/libs/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { AxiosInstance } from 'axios';
 
 const baseURL: string =
-  import.meta.env.MODE === 'development' ? 'http://localhost:5001/api' : '/api';
+  import.meta.env.MODE === 'development' ? 'http://localhost:5001/api' : 'https://softech-project-2.onrender.com/api';
 
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL,
