7c9843256e06733f47075db479fcb326e35c265a
Aaryashkc <aaryashkc123@gmail.com>
2025-10-17 19:20:36 +0545

    changes

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: "50mb" })); 
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors({
-    origin: ["http://localhost:5173", "https://softech-project-2.vercel.app"],
+    origin: ["http://localhost:5173", "https://softech-project-2.vercel.app", "https://ranjittamang.com"],
     credentials: true, 
 }
 ))
