522fb4c3ea9ce49bc6dd8fed9ded48ff97c89f74
Aaryashkc <aaryashkc123@gmail.com>
2025-11-09 14:15:14 +0545

    push

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: "500mb" })); 
 app.use(express.urlencoded({ limit: "500mb", extended: true }));
 app.use(cors({
-    origin: ["http://localhost:5173", "https://softech-project-2.vercel.app", "https://ranjittamang.com", "https://www.ranjittamang.com"],
+    origin: ["http://localhost:5173", "https://softech-project-2.vercel.app", "https://ranjittamang.com", "https://www.ranjittamang.com", "https://www.ranjittamang.com/admin"],
     credentials: true, 
 }
 ))
